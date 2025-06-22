ba8e3649d34cd0d5bb690517c535f80193b87f09
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-22 21:38:05 +0100

    Converted alx-project-0x07 to a normal folder

---
+++ alx-project-0x07/pages/components/layouts/Layout.tsx
@@ -0,0 +1,19 @@
+// Update the import path below to the actual location of your interfaces file.
+// For example, if your interfaces are in 'src/interfaces/index.ts', use the following:
+import { ReactComponentProps } from "../../interfaces/index";
+// Or adjust the path as needed based on your project structure.
+
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout: React.FC<ReactComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
